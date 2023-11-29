0a914f23cc190cff6439cf3cdb336ec9900390ba
Adarsh <adarshofficial.1607@gmail.com>
2023-11-29 14:09:35 +0530

    code formatted

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const winnerName = document.querySelector(".winner");
 const boxes = document.querySelectorAll(".box");
 const boxText = document.querySelectorAll(".box__text");
 const restart = document.querySelector(".restart__btn");
-const gameOverSound = new Audio("./audio/gameOver.mp3")
-const tapSound = new Audio("./audio/tap.mp3")
+const gameOverSound = new Audio("./audio/gameOver.mp3");
+const tapSound = new Audio("./audio/tap.mp3");
 
 let turn = "X";
 
@@ -16,17 +16,17 @@ const changeTurn = () => {
 
 // This is a function to display the result...
 function displayResult() {
-  setTimeout(function(){
+  setTimeout(function () {
     showResult.style.display = "flex";
-  }, 200)
-  gameOverSound.volume = 0.04
-  gameOverSound.currentTime = 0
+  }, 200);
+  gameOverSound.volume = 0.04;
+  gameOverSound.currentTime = 0;
   gameOverSound.play();
   clickedBox = 0;
-  setTimeout(function(){
-    restart.style.transform = "scale(1)"
-    restart.style.opacity = "1"
-  }, 1000)
+  setTimeout(function () {
+    restart.style.transform = "scale(1)";
+    restart.style.opacity = "1";
+  }, 1000);
 }
 
 // This is a function to check for win
@@ -78,27 +78,27 @@ boxes.forEach((element) => {
       winnerName.innerHTML = "Draw!";
       console.log(clickedBox);
     }
-    tapSound.volume = 0.2
-    tapSound.currentTime = 0
-    tapSound.play()
+    tapSound.volume = 0.2;
+    tapSound.currentTime = 0;
+    tapSound.play();
   });
 });
 
 // Adding Game Restart Code...
 restart.addEventListener("click", function () {
   turn = "X";
-  setTimeout(function(){
+  setTimeout(function () {
     showResult.style.display = "none";
-  boxText.forEach((el) => {
-    el.innerHTML = "";
-  });
-  playerTurn.innerHTML = "Turn of " + turn;
-  boxes.forEach((el) => {
-    el.style.cursor = "pointer";
-    el.style.pointerEvents = "auto";
-  });
-  gameOverSound.pause();
-  restart.style.transform = "scale(0)"
-  restart.style.opacity = "1"
-  }, 200)
+    boxText.forEach((el) => {
+      el.innerHTML = "";
+    });
+    playerTurn.innerHTML = "Turn of " + turn;
+    boxes.forEach((el) => {
+      el.style.cursor = "pointer";
+      el.style.pointerEvents = "auto";
+    });
+    gameOverSound.pause();
+    restart.style.transform = "scale(0)";
+    restart.style.opacity = "1";
+  }, 200);
 });
